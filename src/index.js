4f7a3a0644a84c07fd9a81b9aeed18f62d49f70f
Santeri Ruuskanen <81624691+sandelsson@users.noreply.github.com>
2021-09-13 17:49:32 +0300

    Final

---
+++ src/index.js
@@ -0,0 +1,20 @@
+document.getElementById("add-comment").onclick = function () {
+  var node = document.createElement("Li");
+  var text = document.getElementById("user_input").value;
+  var textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  document.getElementById("list").appendChild(node);
+};
+
+document.getElementById("clear-list").onclick = function () {
+  var txt;
+  var r = confirm("Are you sure you want to clear the list?");
+  if (r === true) {
+    txt = "List has been cleared!";
+    var ul = document.getElementById("list");
+    while (ul.firstChild) ul.removeChild(ul.firstChild);
+  } else {
+    txt = "Cancelled!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+};
